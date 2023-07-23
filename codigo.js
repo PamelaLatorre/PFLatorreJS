1c4c5eb1b6dbcd43de45c30946fee8930f4b02b6
pame_latorre <pamelatorre33@gmail.com>
2023-07-22 21:30:07 -0300

    agregamos una funcion de orden superior

--- codigo.js
+++ codigo.js
@@ -1,3 +1,29 @@
+//segundo desafio
+console.table(productos);
+
+//función para filtrar los productos por nombre
+function filtrarPorNombre(nombreBuscado){
+    const filtrados = productos.filter((producto) => producto.nombre.toLowerCase().includes(nombreBuscado.toLowerCase()));
+    // console.log(filtrados);
+    return filtrados;
+}
+
+//datos de entrada por parte del usuario
+let nombreUsuario = prompt("Ingresa el nombre del producto que estas buscando (0-salir)");
+
+while (nombreUsuario != 0){
+    const prodsFiltrados = filtrarPorNombre(nombreUsuario);
+    if (prodsFiltrados.length === 0) {
+        alert("No se encontraron productos con ese nombre 👀.");
+    } else {
+    console.table(prodsFiltrados);
+    }
+    //se vuelve a preguntar
+    nombreUsuario = prompt("Ingresa el nombre del producto que estas buscando (0-salir)");
+}
+
+
+//primer desafio entregado
 let compraTotal = 0;
 
 let producto = prompt("1- Cuadro 13x18cm Plano... $1270\n2- Cuadro 15x21cm Plano... $1380\n3- Cuadro 21x30cm Plano... $2300\n4- Cuadro 30x40cm Plano... $3000\n5- Cuadro 13x18cm Box ... $1500\n6- Cuadro 15x21cm Box ... $1600\n7- Cuadro 20x25cm Box ... $2300\n8- Cuadro 21x30cm Box ... $2700\n0- Para finalizar");
@@ -45,6 +71,7 @@ while(producto != 0){
 
 alert ("El monto total de tu compra es $"+ compraTotal)
 
+//funcion para sumar el total de la compra realizada y la tira por consola
 function sumarCompraTotal (precioCuadro){
     compraTotal = compraTotal + precioCuadro;
     console.log("La compra suma por el momento $" +compraTotal);
---
+++ productos.js
@@ -0,0 +1,72 @@
+const productos =[{
+    id: 1,
+    foto: src="../assets/images/cuadro-abc.jpeg",
+    nombre: "Cuadro infantil",
+    precio: 1600
+},
+{
+    id: 2,
+    foto: src="../assets/images/cuadro-frase1.jpeg",
+    nombre: "Cuadro frase",
+    precio: 1800
+},
+{
+    id: 3,
+    foto: src="../assets/images/cuadro-boho.jpeg",
+    nombre: "Cuadro boho",
+    precio: 2100
+},
+{
+    id: 4,
+    foto: src="../assets/images/cuadro-iniciales.jpeg",
+    nombre: "Cuadro inicial",
+    precio: 1900
+},
+{
+    id: 5,
+    foto: src="../assets/images/cuadro-estampado.jpeg",
+    nombre: "Cuadro estampado",
+    precio: 2500
+},
+{
+    id: 6,
+    foto: src="../assets/images/espejo1.jpeg",
+    nombre: "Espejo",
+    precio: 3100
+},
+{
+    id: 7,
+    foto: src="../assets/images/espejo2.jpeg",
+    nombre: "Espejo infantil",
+    precio: 3400
+},
+{
+    id: 8,
+    foto: src="../assets/images/tapiz3.jpeg",
+    nombre: "Tapiz letra",
+    precio: 1900
+},
+{
+    id: 9,
+    foto: src="../assets/images/tapiz1.jpeg",
+    nombre: "Tapiz animal",
+    precio: 2100
+},
+{
+    id: 10,
+    foto: src="../assets/images/almo1.jpeg",
+    nombre: "Almohadon estampado grande",
+    precio: 3300
+},
+{
+    id: 11,
+    foto: src="../assets/images/almo5.jpeg",
+    nombre: "Almohadon grande",
+    precio: 2900
+},
+{
+    id: 12,
+    foto: src="../assets/images/almo8.jpeg",
+    nombre: "Almohadon estampado chico",
+    precio: 2800
+}]
