ec23e99a5cd1e22f94a8bf5750f159bcceec08e9
pame_latorre <pamelatorre33@gmail.com>
2023-08-05 23:24:32 -0300

    agregamos nav bar y acomodamos cards

--- codigo.js
+++ codigo.js
@@ -1,8 +1,8 @@
 //segundo desafio
-console.table(productos);
+console.table(cuadros, espejos, tapices, almohadones);
 
 //función para filtrar los productos por nombre
-function filtrarPorNombre(nombreBuscado){
+function filtrarPorNombre(nombreBuscado, cuadros, espejos, tapices, almohadones){
     const filtrados = productos.filter((producto) => producto.nombre.toLowerCase().includes(nombreBuscado.toLowerCase()));
     // console.log(filtrados);
     return filtrados;
@@ -12,99 +12,79 @@ function filtrarPorNombre(nombreBuscado){
 let nombreUsuario = prompt("Ingresa el nombre del producto que estas buscando (0-salir)");
 
 while (nombreUsuario != 0){
-    const prodsFiltrados = filtrarPorNombre(nombreUsuario);
-    if (prodsFiltrados.length === 0) {
-        alert("No se encontraron productos con ese nombre ❌");
-    } else {
-    console.table(prodsFiltrados);
-    }
-    //se vuelve a preguntar
-    nombreUsuario = prompt("Ingresa el nombre del producto que estas buscando (0-salir)");
-}
-
-
-//primer desafio entregado
-let compraTotal = 0;
-
-let producto = prompt("1- Cuadro infantil ... $1600\n2- Cuadro frase ... $1800\n3- Cuadro boho ... $2100\n4- Cuadro inicial ... $1900\n5- Cuadro estampado ... $2500\n6- Espejo ... $3100\n7- Espejo infantil ... $3400\n8- Tapiz letra ... $1900\n9- Tapiz animal ... $2100\n10- Almohadon estampado grande ... $3300\n11- Almohadon grande ... $2900\n12- Almohadon estampado chico ... $2800\n0- Para finalizar");
-
-while(producto != 0){
-    switch(producto){
-        case "1" : 
-            alert("Agregaste Cuadro infantil ... $1600 a tu carrito 🛒");
-            sumarCompraTotal (1600);
-        case "2" :
-            alert("Agregaste Cuadro frase ... $1800 a tu carrito 🛒");
-            sumarCompraTotal (1800);
-            break;
-        case "3" :
-            alert("Agregaste Cuadro boho ... $2100 a tu carrito 🛒");
-            sumarCompraTotal (2100);
-            break; 
-        case "4" :
-            alert("Agregaste Cuadro inicial ... $1900 a tu carrito 🛒");
-            sumarCompraTotal (1900);
-            break;
-        case "5" :
-            alert("Agregaste Cuadro estampado ... $2500 a tu carrito 🛒");
-            sumarCompraTotal (2500);
-            break;
-        case "6" :
-            alert("Agregaste Espejo ... $3100 a tu carrito 🛒");
-            sumarCompraTotal (3100);
-            break;
-        case "7" :
-            alert("Agregaste Espejo infantil ... $3400 a tu carrito 🛒");
-            sumarCompraTotal (3400);
-            break;
-        case "8" :
-            alert("Agregaste Tapiz letra ... $1900 a tu carrito 🛒");
-            sumarCompraTotal (1900);
-            break;
-        case "9" :
-            alert("Agregaste Tapiz animal ... $2100 a tu carrito 🛒");
-            sumarCompraTotal (2100);
-            break;
-        case "10" :
-            alert("Agregaste Almohadon estampado grande ... $3300 a tu carrito 🛒");
-            sumarCompraTotal (3300);
-            break;
-        case "11" :
-            alert("Agregaste Almohadon grande ... $2900 a tu carrito 🛒");
-            sumarCompraTotal (2900);
-            break;
-        case "12" :
-            alert("Agregaste Almohadon estampado chico... $2800 a tu carrito 🛒");
-            sumarCompraTotal (2800);
-            break;
-        default :
-            alert("Código de producto incorrecto ❌");
-            break;
-    }
     
-    //se vuelve a preguntar
-    producto = prompt("1- Cuadro infantil ... $1600\n2- Cuadro frase ... $1800\n3- Cuadro boho ... $2100\n4- Cuadro inicial ... $1900\n5- Cuadro estampado ... $2500\n6- Espejo ... $3100\n7- Espejo infantil ... $3400\n8- Tapiz letra ... $1900\n9- Tapiz animal ... $2100\n10- Almohadon estampado grande ... $3300\n11- Almohadon grande ... $2900\n12- Almohadon estampado chico ... $2800\n0- Para finalizar");
+    //filtrar y mostrar los productos de cada array
+    console.log("Cuadros:");
+    console.table(filtrarPorNombre(nombreUsuario, cuadros));
+
+    console.log("Espejos:");
+    console.table(filtrarPorNombre(nombreUsuario, espejos));
+
+    console.log("Tapices:");
+    console.table(filtrarPorNombre(nombreUsuario, tapices));
+
+    console.log("Almohadones:");
+    console.table(filtrarPorNombre(nombreUsuario, almohadones));
+
+    //pedimos nombre de producto nuevamente
+    nombreUsuario = prompt("Ingresa el nombre del producto que estás buscando (0 para salir)");
 }
 
-alert ("El monto total de tu compra es $"+ compraTotal)
-
-//funcion para sumar el total de la compra realizada y la tira por consola
-function sumarCompraTotal (precioProducto){
-    compraTotal = compraTotal + precioProducto;
-    console.log("La compra suma por el momento $" +compraTotal);
-}
+console.log("¡Gracias por tu visita!");
 
 //incorporamos cards de Bootstrap
-let articuloCartas = document.getElementById("cartas");
+let contenedorCuadro = document.getElementById("listaCuadros");
 
-for (const producto of productos){
-    articuloCartas.innerHTML += `
-    <div class="card" style="width: 18rem;">
+for (const producto of cuadros){
+    contenedorCuadro.innerHTML += `
+    <div class="card col-sm-2">
         <img src= "${producto.foto}">
         <div class="card-body">
             <h5 class="card-title">${producto.nombre}</h5>
             <p class="card-text">Precio $ ${producto.precio}</p>
-            <a href="#" class="btn btn-primary">Comprar</a>
+            <button id=${producto.id} class="btn btn-primary">Comprar</button>
+        </div>
+    </div>
+`}
+
+let contenedorEspejo = document.getElementById("listaEspejos");
+
+for (const producto of espejos){
+    contenedorEspejo.innerHTML += `
+    <div class="card col-sm-2">
+        <img src= "${producto.foto}">
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">Precio $ ${producto.precio}</p>
+            <button id=${producto.id} class="btn btn-primary">Comprar</button>
+        </div>
+    </div>
+`}
+
+let contenedorTapiz = document.getElementById("listaTapices");
+
+for (const producto of tapices){
+    contenedorTapiz.innerHTML += `
+    <div class="card col-sm-2">
+        <img src= "${producto.foto}">
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">Precio $ ${producto.precio}</p>
+            <button id=${producto.id} class="btn btn-primary">Comprar</button>
+        </div>
+    </div>
+`}
+
+let contenedorAlmoha = document.getElementById("listaAlmoha");
+
+for (const producto of almohadones){
+    contenedorAlmoha.innerHTML += `
+    <div class="card col-sm-2">
+        <img src= "${producto.foto}">
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">Precio $ ${producto.precio}</p>
+            <button id=${producto.id} class="btn btn-primary">Comprar</button>
         </div>
     </div>
 `}
--- productos.js
+++ productos.js
@@ -1,4 +1,4 @@
-const productos =[{
+const cuadros =[{
     id: 1,
     foto: "../assets/img/cuadro-abc.jpeg",
     nombre: "Cuadro infantil",
@@ -27,45 +27,48 @@ const productos =[{
     foto: "../assets/img/cuadro-estampado.jpeg",
     nombre: "Cuadro estampado",
     precio: 2500
-},
-{
-    id: 6,
+}]
+
+const espejos =[{
+    id: 1,
     foto: "../assets/img/espejo1.jpeg",
     nombre: "Espejo",
     precio: 3100
 },
 {
-    id: 7,
+    id: 2,
     foto: "../assets/img/espejo2.jpeg",
     nombre: "Espejo infantil",
     precio: 3400
-},
-{
-    id: 8,
+}]
+
+const tapices =[{
+    id: 1,
     foto: "../assets/img/tapiz3.jpeg",
     nombre: "Tapiz letra",
     precio: 1900
 },
 {
-    id: 9,
+    id: 2,
     foto: "../assets/img/tapiz1.jpeg",
     nombre: "Tapiz animal",
     precio: 2100
-},
-{
-    id: 10,
+}]
+
+const almohadones =[{
+    id: 1,
     foto: "../assets/img/almo1.jpeg",
     nombre: "Almohadon estampado grande",
     precio: 3300
 },
 {
-    id: 11,
+    id: 2,
     foto: "../assets/img/almo5.jpeg",
     nombre: "Almohadon grande",
     precio: 2900
 },
 {
-    id: 12,
+    id: 3,
     foto: "../assets/img/almo8.jpeg",
     nombre: "Almohadon estampado chico",
     precio: 2800
