ab4c2a275e8b0984e552127514fa8301959b2eae
pame_latorre <pamelatorre33@gmail.com>
2023-08-21 11:03:58 -0300

    agregamos parrafo a las cards

--- productos.js
+++ productos.js
@@ -27,6 +27,18 @@ const cuadros =[{
     foto: "../assets/img/cuadro-estampado.jpeg",
     nombre: "Cuadro estampado",
     precio: 2500
+},
+{
+    id: 17,
+    foto: "../assets/img/cuadro-duo1.jpeg",
+    nombre: "Duo de cuadros",
+    precio: 3400
+},
+{
+    id: 18,
+    foto: "../assets/img/cuadro-especial.jpeg",
+    nombre: "Cuadro especial - Dia de la Madre",
+    precio: 2900
 }]
 
 const espejos =[{
@@ -53,6 +65,12 @@ const tapices =[{
     foto: "../assets/img/tapiz1.jpeg",
     nombre: "Tapiz animal",
     precio: 2100
+},
+{
+    id: 13,
+    foto: "../assets/img/tapiz4.jpeg",
+    nombre: "Tapiz corazon",
+    precio: 2100
 }]
 
 const almohadones =[{
@@ -64,12 +82,30 @@ const almohadones =[{
 {
     id: 11,
     foto: "../assets/img/almo5.jpeg",
-    nombre: "Almohadon grande",
-    precio: 2900
+    nombre: "Almohadon grande bordado ancho",
+    precio: 3100
 },
 {
     id: 12,
     foto: "../assets/img/almo8.jpeg",
     nombre: "Almohadon estampado chico",
     precio: 2800
+},
+{
+    id: 14,
+    foto: "../assets/img/almo6.jpeg",
+    nombre: "Almohadon grande bordado",
+    precio: 2900
+},
+{
+    id: 15,
+    foto: "../assets/img/almo7.jpeg",
+    nombre: "Almohadon estampado grande",
+    precio: 3300
+},
+{
+    id: 16,
+    foto: "../assets/img/almo9.jpeg",
+    nombre: "Almohadon estampado grande",
+    precio: 3300
 }]
